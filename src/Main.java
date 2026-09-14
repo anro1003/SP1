e70b55e12e4d5f24a0e2116b6291ce007bc2b1bf
anro1003 <anro1003@stud.ek.dk>
2026-09-14 10:33:06 +0200

    Iteration 2

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public static void main(String[] args)
     int heroHP = 85;
     int heroMaxHP = 100;
     int heroLevel = 5;
-    int heroExperiencePoints = 2300;
+    int heroXP = 2300;
     double heroGold = 156.50;
-    boolean isAlive = true;
+    boolean heroIsAlive = true;
     char heroClass = 'W';
+
+    double heroHealthWarningPercentage = 0.25;
 
     String[] heroInventory = {"Sword", "Bow", "Arrow"};
 
@@ -16,13 +18,80 @@ public static void main(String[] args)
     System.out.println("Hero: " + heroName);
     System.out.println("HP: " + heroHP + "/" + heroMaxHP);
     System.out.println("Level: " + heroLevel);
-    System.out.println("Experience: " + heroExperiencePoints);
+    System.out.println("Experience: " + heroXP);
     System.out.println("Gold: " + heroGold);
 
+    System.out.print("Hero Class: ");
+    switch (heroClass)
+    {
+        case 'W':
+            System.out.println("Warrior");
+            break;
+
+        case 'M':
+            System.out.println("Mage");
+            break;
+        case 'R':
+            System.out.println("Rouge");
+            break;
+        default:
+            System.out.println("Error - Unknown class: " + heroClass);
+            break;
+
+    }
+
     System.out.println("Inventory:");
-    System.out.println(" - " + heroInventory[0]);
-    System.out.println(" - " + heroInventory[1]);
-    System.out.println(" - " + heroInventory[2]);
+    System.out.println("You have " + heroInventory.length + " items in your inventory.");
+    for(String item : heroInventory)
+    {
+        System.out.println(" - " + item);
+    }
 
     System.out.println("=======================");
+
+    System.out.println("\n\n===== HERO STATUS =====");
+
+    if(heroHP <= 0)
+    {
+        heroIsAlive = false;
+        System.out.println("The hero is dead..");
+    }
+
+    if(heroHP < (heroMaxHP*heroHealthWarningPercentage) && heroIsAlive)
+    {
+        System.out.println("WARNING: Health critical! - It's " +  heroHealthWarningPercentage*100 + "% of your max hp!");
+    }
+
+
+
+    if(heroXP >= heroXP*1250 && heroIsAlive)
+    {
+        System.out.println("Ready to level up!!");
+    }
+
+
+    if(!heroIsAlive)
+    {
+        System.out.println("The hero is dead.. - Skipping combat simulator");
+        return;
+    }
+
+    System.out.println("\n\n==== COMBAT SIMULATOR ====");
+    System.out.println("Starting combat simulator...");
+
+    int monsterDmg = (int)(Math.random()*100);
+
+    System.out.println("The monster attacks for: " + monsterDmg);
+
+    heroHP -= monsterDmg;
+    if (heroHP <= 0)
+    {
+        heroIsAlive = false;
+        heroHP = 0;
+        System.out.println("You didn't survive the attack..");
+        return;
+    }
+    System.out.println("You survived!! - Your new HP is: " + heroHP);
+
+
 }
